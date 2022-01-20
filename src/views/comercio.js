e119dbd05ecb2a9cbb7807ecb18e35184dd53e75
izaias <zambelestar@gmail.com>
2022-01-20 19:01:38 -0300

    Update comercio.js

--- src/views/comercio.js
+++ src/views/comercio.js
@@ -13,7 +13,7 @@ export default function App() {
        <Text
              style={styles.hyperlinkStyle}
              onPress={() => {
-               Linking.openURL('https://wa.me/+5521973705035/?text=Eu%20tenho%20interesse%20no%20seu%20Produto%20à%20venda');
+               Linking.openURL('https://wa.me/seutelaqui/?text=Eu%20tenho%20interesse%20no%20seu%20Produto%20à%20venda');
              }}>
            Comerciantes do Bairro
            </Text>   
