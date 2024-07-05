2c4fc7e87f8f1d33f8e3e36151b2f99d5bb21ba0
izaias <zambelestar@gmail.com>
2024-07-04 22:25:28 -0300

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -46,7 +46,7 @@ export default function App({ navigation }) {
           style={styles.Image}
           source={require("../../assets/utilidadepublica/index.png")}
         />
-        <Text>Utilidade Publica</Text>
+        <Text>Utilidade Pública</Text>
       </TouchableOpacity>
     </View>
   );
